99af997e3f3a0978dfe5852d958d631821c6f568
Carlos Pieper <capieper.pieper@gmail.com>
2022-06-21 22:14:28 -0300

    first commit

---
+++ escritor/Program.cs
@@ -0,0 +1,44 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Esse é o escritor, aqui você digita algo, para o projeto leitor enviar para o BaaS");
+        var mqttFactory = new MqttFactory();
+        var client = mqttFactory.CreateMqttClient();
+        var options = new MqttClientOptionsBuilder()
+        .WithClientId("teste1")
+        .WithTcpServer("0.0.0.0", 1883)
+        .WithCleanSession()
+        .Build();
+        client.UseConnectedHandler(e =>
+        {
+            Console.WriteLine("conectado");
+        });
+        client.UseDisconnectedHandler(e =>
+        {
+            Console.WriteLine("desconectado");
+        });
+        await client.ConnectAsync(options);
+        Console.WriteLine("digite algo para");
+        var mensagem = Console.ReadLine();
+        Publicar(client, mensagem);
+        await client.DisconnectAsync();
+    }
+
+    private static async Task Publicar(IMqttClient client, string mensagem)
+    {
+        string messagePayload = mensagem;
+        var message = new MqttApplicationMessageBuilder()
+        .WithTopic("trabalho")
+        .WithPayload(messagePayload)
+        .WithAtLeastOnceQoS()
+        .Build();
+        if (client.IsConnected)
+        {
+            await client.PublishAsync(message);
+        }
+    }
+}
---
+++ leitor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Text.Json;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var mqttFactory = new MqttFactory();
+        var client = mqttFactory.CreateMqttClient();
+        var options = new MqttClientOptionsBuilder()
+        .WithClientId("teste2")
+        .WithTcpServer("0.0.0.0", 1883)
+        .WithCleanSession()
+        .Build();
+        client.UseConnectedHandler(async e =>
+        {
+            Console.WriteLine("conectado");
+            var topicFilter = new TopicFilterBuilder()
+            .WithTopic("trabalho")
+            .Build();
+            await client.SubscribeAsync(topicFilter);
+        });
+        client.UseDisconnectedHandler(e =>
+        {
+            Console.WriteLine("desconectado");
+        });
+        client.UseApplicationMessageReceivedHandler(async e =>
+        {
+            Console.WriteLine($"mensagem recebida");
+            var mensagem = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            var url = $"https://interoperabilidade-json-9eaa9-default-rtdb.firebaseio.com/envios/envios.json";
+            var httpClient = new HttpClient();
+            Console.WriteLine("");
+            try
+            {
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(mensagem);
+                var response = httpClient.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json")).Result;
+                Console.WriteLine("Dados enviados para o BaaS");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro");
+            }
+        });
+        await client.ConnectAsync(options);
+        Console.ReadLine();
+        await client.DisconnectAsync();
+    }
+}
